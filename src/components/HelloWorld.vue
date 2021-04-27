2eb337123365da87956f7f85be081f84ed4b79c3
DerZade <DerZade@gmail.com>
2021-04-27 15:26:15 +0200

    Updated es-lint rules

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,12 +33,12 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
+import { Options, Vue } from 'vue-class-component';
 
 @Options({
-  props: {
-    msg: String
-  }
+    props: {
+        msg: String
+    }
 })
 export default class HelloWorld extends Vue {
   msg!: string
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,13 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Options({
-  components: {
-    HelloWorld
-  }
+    components: {
+        HelloWorld
+    }
 })
 export default class Home extends Vue {}
 </script>

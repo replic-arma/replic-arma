4bbcbb17999a2cf29a12971498e435359ccd27fb
RednecksRevenge <deoflasche63@gmail.com>
2021-12-01 22:52:04 +0100

    rust

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,10 @@ module.exports = {
         ecmaVersion: 2020
     },
     rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         indent: ['error', 4],
-        semi: ['error', 'always']
+        semi: 'off',
+        '@typescript-eslint/semi': ['error', 'always'],
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
     }
 };

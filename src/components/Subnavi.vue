a44aba44b747e55d83033459db4962007c544668
RednecksRevenge <deoflasche63@gmail.com>
2021-12-03 18:27:27 +0100

    subnavi Co-authored-by: DerZade <DerZade@gmail.com>

--- src/components/Subnavi.vue
+++ src/components/Subnavi.vue
@@ -7,7 +7,7 @@
 </template>
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import { Prop } from 'vue-property-decorator';
+import { Prop, Watch } from 'vue-property-decorator';
 export interface SubnaviItem {
     label: string;
     link: string;
@@ -16,31 +16,69 @@ export interface SubnaviItem {
 @Options({
     components: { }
 })
-export default class Subnavi extends Vue {
+export default class SubnaviVue extends Vue {
     @Prop({ type: Array }) private subnaviItems!: SubnaviItem[];
+
+    public created () {
+        this.onRouteChanged();
+    }
+
+    @Watch('$route')
+    private onRouteChanged () {
+        this.$nextTick(() => {
+            const anchor = this.$el.querySelector('.router-link-active') as HTMLAnchorElement;
+            const el = anchor.parentElement as HTMLLIElement;
+
+            const bb = el.getBoundingClientRect();
+            const parentBB = this.$el.getBoundingClientRect();
+
+            const left = bb.left - parentBB.left;
+            const width = bb.width;
+            (this.$el as HTMLUListElement).style.setProperty('--subnavi-left', `${left}px`);
+            (this.$el as HTMLUListElement).style.setProperty('--subnavi-width', `${width}px`);
+        });
+    }
 }
 </script>
 <style lang="scss" scoped>
 .subnavi {
     list-style-type: none;
+    border-bottom: 1px solid #333333;
+    display: flex;
+    padding: 0 0 1rem 0;
+    position: relative;
+    user-select: text;
+
     &__item {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        font-style: normal;
-        font-weight: 500;
-        font-size: 18px;
-        line-height: 21px;
-        color: #333333;
-        float: left;
-        width: 98px;
-        height: 37px;
-        padding: 8px 12px;
-        border-radius: 12px;
-        &:hover {
-            background: rgba(0, 0, 0, 0.1);
+        z-index: 1;
+
+        > a {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            font-size: 16pt;
+            color: var(--c-text-1);
+            height: 1.5rem;
+            padding: .75rem .5rem;
+            border-radius: .25rem;
         }
     }
+
+    &::before {
+        block-size: 3.25rem;
+        content: '';
+        position: absolute;
+        inset-block: 0;
+        inset-inline-start: var(--subnavi-left, 0);
+        inline-size: var(--subnavi-width, 0px);
+        background: var(--c-surf-3);
+        border-radius: 1rem;
+        z-index: 0;
+        transition: all 0.07s ease-out;
+    }
+}
+a:not(.router-link-active) {
+    color: var(--c-text-3);
 }
 </style>

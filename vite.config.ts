c07092fdf1dbd011a550e42a29ffc44d5ec8590c
RednecksRevenge <32557520+RednecksRevenge@users.noreply.github.com>
2023-09-21 19:45:24 +0200

    fix> ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,34 @@
-import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'url';
-import { defineConfig } from 'vite';
-import tauriPlugin from './vite-plugin-tauri';
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), tauriPlugin()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    define: {
-        __VUE_I18N_FULL_INSTALL__: true,
-        __VUE_I18N_LEGACY_API__: false,
-        __INTLIFY_PROD_DEVTOOLS__: false
-    }
+  },
+  define: {
+    __VUE_I18N_FULL_INSTALL__: true,
+    __VUE_I18N_LEGACY_API__: false,
+    __INTLIFY_PROD_DEVTOOLS__: false,
+  }, // prevent vite from obscuring rust errors
+  clearScreen: false,
+  // Tauri expects a fixed port, fail if that port is not available
+  server: {
+    strictPort: true,
+  },
+  // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
+  // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
+  // env variables
+  envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
 });
